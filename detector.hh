27689164808208eef9793f56644b2fbd165adb9f
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-03-04 22:38:46 +0100

    Update detector.hh

--- detector.hh
+++ detector.hh
@@ -6,6 +6,7 @@
 #include "g4root_defs.hh" // ok with geant version 11.1
 #include "G4RunManager.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4PhysicsOrderedFreeVector.hh"
 
 
 class MySensitiveDetector : public G4VSensitiveDetector
