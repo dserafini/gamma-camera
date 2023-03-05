0d1254db338be96e052e89781cfb5ea901d57735
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-03-05 13:45:16 +0100

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -127,8 +127,8 @@ void MyDetectorConstruction::ConstructCollimator()
 	G4cout << "MyDetectorConstruction::ConstructCollimator" << G4endl;
 	// Collimator parameters
 	hole_length = 30.*mm;
-	septa_thickness = 20.*mm; // 2*mm;
-	hole_thickness = 30.*mm; // 3*mm;
+	septa_thickness = 10.*mm; // 2*mm;
+	hole_thickness = 20.*mm; // 3*mm;
 	case_side = 10.*cm; // fixed but not necessarily precise
 	
 	// Derived parameters
