624af4705c91f8ee366b522dfd65bd4de498ddd0
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-03-05 15:53:04 +0100

    retry defining GAGG  material

--- construction.cc
+++ construction.cc
@@ -64,8 +64,8 @@ void MyDetectorConstruction::DefineMaterials()
 	// GaGG
 	// the difference between the types of GaGGs involves only the properties and not the constituents
 	G4cout << "Define Material GAGG" << G4endl;
-	// materialGAGG = new G4Material("materialGAGG", 6.6*g/cm3, 1);
-	materialGAGG = nist->FindOrBuildMaterial("G4_BGO");
+	materialGAGG = new G4Material("materialGAGG", 6.6*g/cm3, 1);
+	// materialGAGG = nist->FindOrBuildMaterial("G4_BGO");
 	G4cout << "Add Element Gd" << G4endl;
 	// materialGAGG->AddElement(nist->FindOrBuildElement("G4_Gd"), 3);
 	G4cout << "Add Element Al" << G4endl;
