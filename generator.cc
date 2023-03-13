ec2d0ca10176ecdcadd465e91db50cd32c2bd319
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-03-13 16:01:02 +0100

    Update generator.cc

--- generator.cc
+++ generator.cc
@@ -3,12 +3,6 @@
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
 	fParticleGPS = new G4GeneralParticleSource();
-	fMessenger = new G4GenericMessenger(this, "/source/", "Source definition");
-	fMessenger->DeclarePropertyWithUnit("sourceDistance", "mm", sourceDistance, "Distance source-collimator");
-	sourceDistance = 30.*mm;
-	
-	G4UImanager * uiManager = G4UImanager::GetUIpointer();
-	uiManager->ApplyCommand("/gps/position 0 0 -5 cm");
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
