a8acc034992ea32852734261e1f5deb7a9f65030
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-08 11:45:22 +0200

    Update event.cc

--- event.cc
+++ event.cc
@@ -18,7 +18,7 @@ void MyEventAction::BeginOfEventAction(const G4Event*)
 
 void MyEventAction::EndOfEventAction(const G4Event*)
 {
-  // G4cout << "Energy deposition: " << fEdep << " keV" << G4endl;
+  G4cout << "Energy deposition: " << fEdep << " keV" << G4endl;
 
   G4AnalysisManager *man = G4AnalysisManager::Instance();
 
