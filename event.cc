fd03be76e7a10fe8c2e92fa90ee21ce9b5da51c5
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-15 17:45:02 +0200

    fill ntuple about optical photons

--- event.cc
+++ event.cc
@@ -2,8 +2,14 @@
 
 MyEventAction::MyEventAction(MyRunAction*)
 {
+  // gammas
   fEdep = 0.;
   fPosition = G4ThreeVector(0.,0.,0.);
+  
+  // optical photons
+  pNumber = 0.;
+  pPosition = G4ThreeVector(0.,0.,0.);
+  pSigma = G4ThreeVector(0.,0.,0.);
 }
 
 MyEventAction::~MyEventAction()
@@ -14,6 +20,9 @@ void MyEventAction::BeginOfEventAction(const G4Event*)
   // whenever a new event starts the energy accumulated in the previous event should be set to 0
   fEdep = 0.;
   fPosition = G4ThreeVector(0.,0.,0.);
+  pNumber = 0.;
+  pPosition = G4ThreeVector(0.,0.,0.);
+  pSigma = G4ThreeVector(0.,0.,0.);
 }
 
 void MyEventAction::EndOfEventAction(const G4Event*)
@@ -22,6 +31,7 @@ void MyEventAction::EndOfEventAction(const G4Event*)
 
   G4AnalysisManager *man = G4AnalysisManager::Instance();
 
+  // gammas
   man->FillNtupleDColumn(0, 0, fEdep);
   
   if (fEdep>0)
@@ -30,6 +40,20 @@ void MyEventAction::EndOfEventAction(const G4Event*)
   man->FillNtupleDColumn(0, 1, fPosition.getX());
   man->FillNtupleDColumn(0, 2, fPosition.getY());
   man->FillNtupleDColumn(0, 3, fPosition.getZ());
+  
+  // optical photons
+  man->FillNtupleIColumn(0, 4, pNumber);
+  
+  if (pNumber>0)
+  {
+    pPosition = pPosition/pNumber; // normalize on the number of optical photons
+    pSigma = pSigma/pNumber;
+  }
+  
+  man->FillNtupleDColumn(0, 5, pPosition.getX());
+  man->FillNtupleDColumn(0, 6, pSigma.getX());
+  man->FillNtupleDColumn(0, 7, pPosition.getY());
+  man->FillNtupleDColumn(0, 8, pSigma.getY());
 
   man->AddNtupleRow(0);
 }
