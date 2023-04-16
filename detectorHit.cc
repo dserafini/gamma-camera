25235872cb9e971e78ea23cb0a8fa1aee22252eb
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-16 16:57:48 +0200

    Update detectorHit.cc

--- detectorHit.cc
+++ detectorHit.cc
@@ -18,23 +18,6 @@ G4bool detectorHit::operator==(const detectorHit& right) const
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-void detectorHit::Draw()
-{
-  G4VVisManager* pVVisManager = G4VVisManager::GetConcreteInstance();
-  if(pVVisManager)
-  {
-    G4Circle circle(fPos);
-    circle.SetScreenSize(4.);
-    circle.SetFillStyle(G4Circle::filled);
-    G4Colour colour(1.,0.,0.);
-    G4VisAttributes attribs(colour);
-    circle.SetVisAttributes(attribs);
-    pVVisManager->Draw(circle);
-  }
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 void detectorHit::Print()
 {
   G4cout
