49a3d11bcd5adad78df6948897f7975d83fc1d4f
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-16 17:15:35 +0200

    Update detectorSD.cc

--- detectorSD.cc
+++ detectorSD.cc
@@ -68,7 +68,7 @@ G4bool MySensitiveDetector::ProcessHits(G4Step* aStep, G4TouchableHistory*)
   // energy deposit
   G4double edep = aStep->GetTotalEnergyDeposit();
 
-  if (edep==0.) return false;
+  // if (edep==0.) return false;
 
   auto newHit = new detectorHit();
 
