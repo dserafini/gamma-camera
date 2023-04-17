6888ca725b9ac98008e864537c9d194828ec0cd4
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-17 15:54:50 +0200

    Update event.cc

--- event.cc
+++ event.cc
@@ -17,7 +17,7 @@ MyEventAction::~MyEventAction()
 
 void MyEventAction::BeginOfEventAction(const G4Event*)
 {
-  G4cout << "MyEventAction::BeginOfEventAction" << G4endl;
+  // G4cout << "MyEventAction::BeginOfEventAction" << G4endl;
   
   // whenever a new event starts the energy accumulated in the previous event should be set to 0
   fEdep = 0.;
@@ -29,7 +29,7 @@ void MyEventAction::BeginOfEventAction(const G4Event*)
 
 void MyEventAction::EndOfEventAction(const G4Event*)
 {
-  G4cout << "MyEventAction::EndOfEventAction" << G4endl;
+  // G4cout << "MyEventAction::EndOfEventAction" << G4endl;
   // G4cout << "Energy deposition: " << fEdep << " keV" << G4endl;
 
   G4AnalysisManager *man = G4AnalysisManager::Instance();
