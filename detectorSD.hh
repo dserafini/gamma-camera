631935b82bd20d0f1904d835b4f0fbef0496b75f
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-17 12:26:20 +0200

    Update detectorSD.hh

--- detectorSD.hh
+++ detectorSD.hh
@@ -2,8 +2,8 @@
 #define DETECTORSD_HH
 
 #include "G4VSensitiveDetector.hh"
-// #include "g4root.hh" // ok with geant version 10.7
-#include "g4root_defs.hh" // ok with geant version 11.1
+#include "g4root.hh" // ok with geant version 10.7
+// #include "g4root_defs.hh" // ok with geant version 11.1
 #include "G4RunManager.hh"
 #include "G4SDManager.hh"
 #include "G4SystemOfUnits.hh"
