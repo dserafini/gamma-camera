e50768d08a1afc9b033ee994b47a0074909ee3bb
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-18 17:46:29 +0200

    Update stepping.cc

--- stepping.cc
+++ stepping.cc
@@ -19,8 +19,10 @@ void MySteppingAction::UserSteppingAction(const G4Step *step)
 
   // check if the volume where the step is in is also our scoring volume
   const MyDetectorConstruction *detectorConstruction = static_cast<const MyDetectorConstruction*> (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
-
-  // G4cout << "collimator is mother? " << volume->IsDaughter(detectorConstruction->GetCollimatorPhysVolume()) << G4endl;
+  
+  if(step->GetTrack()->GetParticleDefinition() == G4Gamma::Definition())
+    G4cout << "collimator is mother? " << volume->IsDaughter(detectorConstruction->GetCollimatorPhysVolume()) << G4endl;
+  
   if((volume->IsDaughter(detectorConstruction->GetCollimatorPhysVolume())) && 
      (step->GetTrack()->GetParticleDefinition() == G4Gamma::Definition()) && 
      (fEventAction->GetCross() < 1))
