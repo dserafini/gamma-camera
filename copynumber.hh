757fcc5bb00ba1913f63437f6c0c335be19fe055
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-24 11:29:36 +0200

    Update copynumber.hh

--- copynumber.hh
+++ copynumber.hh
@@ -11,21 +11,22 @@ public:
   void SetMaxY(G4double valueY) { fMaxY = valueY; };
   void SetMaxNoX(G4int valueX) { fMaxNoX = valueX; };
   void SetMaxNoY(G4int valueY) { fMaxNoY = valueY; };
-  void SetCopyNoX(G4double);
-  void SetCopyNoY(G4double);
-  void UpdateCopyNo();
-  G4int GetCopyNoX() { return fCopyNoX; };
-  G4int GetCopyNoY() { return fCopyNoY; };
-  G4int GetCopyNo() { return fCopyNo; };
+  G4int GetCopyNo(G4double, G4double);
 
 private:
+  void SetCopyNoX(G4double valueX); { fCopyNoX = GetCopyNoForSquareSide(valueX, fMaxX, fMaxNoX); };
+  void SetCopyNoY(G4double valueY); { fCopyNoY = GetCopyNoForSquareSide(valueY, fMaxY, fMaxNoY); };
+  G4int GetCopyNoForSquareSide(G4double, G4double, G4int);
+  void UpdateCopyNo();
+  // G4int GetCopyNoX() { return fCopyNoX; };
+  // G4int GetCopyNoY() { return fCopyNoY; };
   G4int fMaxNoX;
   G4int fMaxNoY;
+  G4double fMaxX;
+  G4double fMaxY;
   G4int fCopyNoX;
   G4int fCopyNoY;
   G4int fCopyNo;
-  G4double fMaxX;
-  G4double fMaxY;
 };
 
 #endif
