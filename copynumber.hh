2fa1739ed3b517b22351be599842c267cf9b63ec
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-04-24 10:56:40 +0200

    Create copynumber.hh

---
+++ copynumber.hh
@@ -0,0 +1,29 @@
+#ifndef COPYNUMBER_HH
+#define COPYNUMBER_HH
+
+class MyCopyNumber
+{
+public:
+	MyCopyNumber();
+	~MyCopyNumber();
+
+  void SetMaxX(G4double valueX) { fMaxX = valueX; };
+  void SetMaxY(G4double valueY) { fMaxY = valueY; };
+  void SetCopyNoX(G4double);
+  void SetCopyNoY(G4double);
+  void UpdateCopyNo();
+  G4int GetCopyNoX() { return fCopyNoX; };
+  G4int GetCopyNoY() { return fCopyNoY; };
+  G4int GetCopyNo() { return fCopyNo; };
+
+private:
+  G4int fMaxNoX;
+  G4int fMaxNoY;
+  G4int fCopyNoX;
+  G4int fCopyNoY;
+  G4int fCopyNo;
+  G4double fMaxX;
+  G4double fMaxY;
+};
+
+#endif
