0055f38f6ded511ad313d3eca6a5da42deaeba91
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-15 17:40:02 +0200

    Update detectorSD.cc

--- detectorSD.cc
+++ detectorSD.cc
@@ -111,8 +111,7 @@ void MySensitiveDetector::EndOfEvent(G4HCofThisEvent*)
   man->FillNtupleDColumn(0, 6, fMeanPos.getY());
   man->FillNtupleDColumn(0, 7, fMeanPos.getZ());
   man->FillNtupleDColumn(0, 8, fSigmaPos.getX());
-  man->FillNtupleDColumn(0, 9, fSigmaPos.getY());
-  man->FillNtupleDColumn(0, 10, fSigmaMod);
+  man->FillNtupleDColumn(0, 9, fSigmaMod);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
