1144134224ca7d9322655083695c648d9d4d1706
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-16 10:50:47 +0200

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -369,7 +369,7 @@ void MyDetectorConstruction::DefineOpticalSurfaceProperties()
 	
 	// block optical photons escaping toward the detector
 	G4OpticalSurface* opGaggDetectorSurface = new G4OpticalSurface("opGaggDetectorSurface");
-	opGaggDetectorSurface->SetMaterialPropertiesTable(MPTabsorbing);
+	opGaggDetectorSurface->SetMaterialPropertiesTable(MPTfresnel);
 	new G4LogicalBorderSurface("logicBorderGaggDetectorSurface", 
 				   physScintillatorPinhole, physDetector, opGaggDetectorSurface);
 				   
