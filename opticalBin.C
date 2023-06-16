005005ce083c00a15a3adfa1c39fff5eeed6d75c
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-16 15:55:32 +0200

    Update opticalBin.C

--- opticalBin.C
+++ opticalBin.C
@@ -30,10 +30,12 @@ void opticalBin(TString fileName, Double_t scinti_hole_thickness = 8., Double_t 
 	t1->Draw("fY:fX>>h2","fEdep>0", "colz");
 	c1->SaveAs("c1"+fileName+"GammaPos.png");
 	    
+	TCanvas * c2 = (TCanvas*)c1->Clone("c2");
+	c2->cd(1);
 	TH2F* h2b = (TH2F*)h2->Clone("h2b");
 	h2b->SetTitle(fileName + " gamma pMeanY:pMeanX");
 	t1->Draw("pMeanY:pMeanX>>h2","fEdep>0", "colz");
-	c1->SaveAs("c1"+fileName+"OpticalPos.png");
+	c2->SaveAs("c2"+fileName+"OpticalPos.png");
     }
   }
 }
