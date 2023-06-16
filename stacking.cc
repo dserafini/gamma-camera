0f9f43703a1f9fcc39f606f3560612f5ae34153d
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-16 19:22:04 +0200

    Update stacking.cc

--- stacking.cc
+++ stacking.cc
@@ -7,7 +7,7 @@ MyStackingAction::~MyStackingAction() {}
 G4ClassificationOfNewTrack MyStackingAction::ClassifyNewTrack(
   const G4Track* aTrack)
 {
-  G4cout << "MyStackingAction::ClassifyNewTrack" << G4endl;
+  // G4cout << "MyStackingAction::ClassifyNewTrack" << G4endl;
   
   if(aTrack->GetParentID() == 1) // Ag-111 daughter
     {
