b5026c6b1ad4bb84b0cbb5e0449209a8824b6d1f
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-16 16:43:09 +0200

    Binning root2 (#23)
    * Create collimatorBin.C
    
    * Update collimatorBin.C
    
    * Update and rename opticalBin.C to scintiBin.C
    
    * Update and rename collimatorBin.C to collBin.C

---
+++ collBin.C
@@ -0,0 +1,41 @@
+void collBin(TString fileName, Double_t coll_hole_thickness = 8., Double_t coll_septa_thickness = 2., Double_t case_side = 100., Int_t coll_holes_number = 10)
+{
+  TFile* file = new TFile(fileName);
+  if (!file) cout << "No file found!" << endl;
+  else
+  {
+    if (fileName.EndsWith(".root"))
+      fileName.Resize(fileName.Length()-5);
+    
+    TTree* t1 = (TTree*)file->Get("Scoring");
+    if (!t1) cout << "No tree found!" << endl;
+    else
+    {
+      TCanvas * c1 = new TCanvas("c1","c1",10,10,800,800);
+      c1->cd(1);
+      
+      // calculate number of bins
+      Double_t coll_pixel_size = coll_hole_thickness + coll_septa_thickness;
+      coll_holes_number = (Int_t) case_side / coll_pixel_size;
+      if ((coll_holes_number % 2) < 1)
+        coll_holes_number = coll_holes_number - 1;
+	    case_side = (Double_t) coll_pixel_size * coll_holes_number;
+      Double_t half_case_side = case_side/2.;
+      
+	TH2F* h2 = new TH2F("h2","h2",coll_holes_number,-half_case_side,half_case_side,coll_holes_number,-half_case_side,half_case_side);
+	h2->GetXaxis()->SetTitle("x [mm]");
+	h2->GetYaxis()->SetTitle("y [mm]");
+	h2->SetTitle(fileName + " gamma fY:fX");
+	cout << "coll_pixel_size: " << h2->ProfileX()->GetBinWidth(1) << " mm" << endl;
+	t1->Draw("fY:fX>>h2","fEdep>0", "colz");
+	c1->SaveAs("c1collBin"+fileName+"GammaPos.png");
+	    
+      	TCanvas * c2 = new TCanvas("c2","c2",10,10,800,800);
+	c2->cd(1);
+	TH2F* h2b = new TH2F("h2b","h2b",coll_holes_number,-half_case_side,half_case_side,coll_holes_number,-half_case_side,half_case_side);
+	h2b->SetTitle(fileName + " optical pMeanY:pMeanX");
+	t1->Draw("pMeanY:pMeanX>>h2b","fEdep>0", "colz");
+	c2->SaveAs("c2collBin"+fileName+"OpticalPos.png");
+    }
+  }
+}
--- scintiBin.C
+++ scintiBin.C
@@ -1,4 +1,4 @@
-void opticalBin(TString fileName, Double_t scinti_hole_thickness = 8., Double_t scinti_septa_thickness = 2., Double_t case_side = 100., Int_t scinti_holes_number = 10)
+void scintiBin(TString fileName, Double_t scinti_hole_thickness = 8., Double_t scinti_septa_thickness = 2., Double_t case_side = 100., Int_t scinti_holes_number = 10)
 {
   TFile* file = new TFile(fileName);
   if (!file) cout << "No file found!" << endl;
@@ -28,14 +28,14 @@ void opticalBin(TString fileName, Double_t scinti_hole_thickness = 8., Double_t 
 	h2->SetTitle(fileName + " gamma fY:fX");
 	cout << "scinti_pixel_size: " << h2->ProfileX()->GetBinWidth(1) << " mm" << endl;
 	t1->Draw("fY:fX>>h2","fEdep>0", "colz");
-	c1->SaveAs("c1"+fileName+"GammaPos.png");
+	c1->SaveAs("c1scintiBin"+fileName+"GammaPos.png");
 	    
       	TCanvas * c2 = new TCanvas("c2","c2",10,10,800,800);
 	c2->cd(1);
 	TH2F* h2b = new TH2F("h2b","h2b",scinti_holes_number,-half_case_side,half_case_side,scinti_holes_number,-half_case_side,half_case_side);
 	h2b->SetTitle(fileName + " optical pMeanY:pMeanX");
 	t1->Draw("pMeanY:pMeanX>>h2b","fEdep>0", "colz");
-	c2->SaveAs("c2"+fileName+"OpticalPos.png");
+	c2->SaveAs("c2scintiBin"+fileName+"OpticalPos.png");
     }
   }
 }
