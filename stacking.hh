6004f30f348c35ce311abd2667db32ffba9c6022
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-17 14:58:06 +0200

    Update stacking.hh

--- stacking.hh
+++ stacking.hh
@@ -4,6 +4,7 @@
 #include "globals.hh"
 #include "g4root.hh"
 #include "G4UserStackingAction.hh"
+#include "generator.hh"
 #include "G4Track.hh"
 #include "G4Electron.hh"
 #include "G4AntiNeutrinoE.hh"
@@ -13,7 +14,7 @@
 class MyStackingAction : public G4UserStackingAction
 {
  public:
-  MyStackingAction();
+  MyStackingAction(MyPrimaryGenerator* aGenerator);
   ~MyStackingAction();
 
   G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track* aTrack) override;
