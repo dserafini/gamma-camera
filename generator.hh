1a4d0070aeac66248d2b09d7a95bc337e666d7d5
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-17 15:15:51 +0200

    Update generator.hh

--- generator.hh
+++ generator.hh
@@ -17,7 +17,7 @@ public:
 	~MyPrimaryGenerator();
 
 	virtual void GeneratePrimaries(G4Event*);
-	G4GeneralParticleSource* GetGPS() { return fParticleGPS; };
+	G4GeneralParticleSource* GetPrimaryGPS() { return fParticleGPS; };
 
 private:
 	G4GeneralParticleSource* fParticleGPS;
