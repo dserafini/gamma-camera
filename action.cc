ed369d68e28b1dee5765ea2aa001602f08e88a4c
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-17 15:23:09 +0200

    Update action.cc

--- action.cc
+++ action.cc
@@ -17,18 +17,15 @@ void MyActionInitialization::Build() const
 	MyPrimaryGenerator *generator = new MyPrimaryGenerator();
 	SetUserAction(generator);
 
-	MyRunAction *runAction = new MyRunAction();
+	MyStackingAction *stackingAction = new MyStackingAction();
+	SetUserAction(stackingAction);
+
+	MyRunAction *runAction = new MyRunAction(generator, stackingAction);
 	SetUserAction(runAction);
-	// we are telling Geant4 that we do not use any default run action
-	// instead we want to use our own run action
-	// same for all the other user defined actions
 
 	MyEventAction *eventAction = new MyEventAction(runAction);
 	SetUserAction(eventAction);
 
 	MySteppingAction *steppingAction = new MySteppingAction(eventAction);
 	SetUserAction(steppingAction);
-
-	MyStackingAction *stackingAction = new MyStackingAction(generator);
-	SetUserAction(stackingAction);
 }
