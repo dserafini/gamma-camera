25df320bfc00a7227a5ccf3619eef58a6cf2c4b7
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-18 11:21:04 +0200

    Update stacking.cc

--- stacking.cc
+++ stacking.cc
@@ -2,70 +2,48 @@
 
 MyStackingAction::MyStackingAction()
 {
-	G4cout << "ClassifyNewTrack is set on aTrack->GetParentID() == 1" << G4endl;
-  
-	fMessengerPrimary = new G4GenericMessenger(this, "/primary/", "Primary selection for stacking action");
-	
-	fMessengerPrimary->DeclareProperty("ion", fIonIsPrimary, "0 gamma, 1 ion");
-
-	fIonIsPrimary = 0;
+	G4cout << "MyStackingAction::MyStackingAction" << G4endl;
 }
 
-MyStackingAction::~MyStackingAction()
-{
-  delete fMessengerPrimary;
-}
+MyStackingAction::~MyStackingAction() {}
 
 G4ClassificationOfNewTrack MyStackingAction::ClassifyNewTrack(
-  const G4Track* aTrack)
+	const G4Track* aTrack)
 {
-  G4cout << "MyStackingAction::ClassifyNewTrack" << G4endl;
-G4cout << "Particle: " << aTrack->GetParticleDefinition()->GetParticleName() << G4endl;
-G4cout << "Kinetic energy: " << aTrack->GetKineticEnergy() / keV << " keV" << G4endl;
-if (aTrack->GetCreatorProcess())
-	G4cout << "Creator process " << aTrack->GetCreatorProcess()->GetProcessName() << G4endl;
-else
-	G4cout << "No creator process" << G4endl;
-
-G4AnalysisManager *man = G4AnalysisManager::Instance();
-
-if (fIonIsPrimary == 1)
-{
-G4cout << "save primary ion gamma daughter energy" << G4endl;
-  if(aTrack->GetParentID() == 1) // Ag-111 daughter
-    {
-    
-      // particle is electron
-      if(aTrack->GetDefinition() == G4Electron::Definition())
-        return fKill;
-      
-      // particle is anti_nu_e
-      if(aTrack->GetDefinition() == G4AntiNeutrinoE::Definition())
-            return fKill;
-    }
-  
-  if(aTrack->GetParentID() == 2) // excited Cd-111 daughter
-    {
-      // G4cout << "figlia di Cd-111" << G4endl;
-      // particle is gamma
-      if(aTrack->GetDefinition() == G4Gamma::Definition())
-      {
-        // G4cout << "gamma e figlia di Cd-111" << G4endl;
-        // G4cout << "energy: " << aTrack->GetKineticEnergy()/keV << " keV" << G4endl;
-        man->FillNtupleDColumn(0, 0, aTrack->GetKineticEnergy()/keV); // [keV]
-      }
-    }
-}
-
-if (fIonIsPrimary == 0)
-{
-	// gammas and primaries
-	if(aTrack->GetDefinition() == G4Gamma::Definition() && aTrack->GetParentID() == 0)
+	// G4cout << "MyStackingAction::ClassifyNewTrack" << G4endl;
+	G4cout << "Particle: " << aTrack->GetParticleDefinition()->GetParticleName() << ", ";
+	if (aTrack->GetParticleDefinition() == G4OpticalPhoton::Definition())
 	{
-		G4cout << "save primary gamma energy" << G4endl;
-		man->FillNtupleDColumn(0, 0, aTrack->GetKineticEnergy()/keV);
+		G4cout << G4endl;
+		return;
+	}
+	else
+	{
+		G4cout << "Kinetic energy: " << aTrack->GetKineticEnergy() / keV << " keV" << G4endl;
+		
+		G4VProcess *aProcess = aTrack->GetCreatorProcess();
+		G4String aProcessName = "none";
+		if (!aProcess)
+			aProcessName = aProcess->GetProcessName();
+		
+		G4AnalysisManager *man = G4AnalysisManager::Instance();
+		
+		// particle is beta-
+		if(aTrack->GetDefinition() == G4Electron::Definition() && aProcessName == "G4RadioactiveDecayBase")
+			return fKill;
+		
+		// particle is anti_nu_e
+		if(aTrack->GetDefinition() == G4AntiNeutrinoE::Definition())
+			return fKill;
+		
+		// particle is gamma, primary or from de-excitation
+		if(aTrack->GetDefinition() == G4Gamma::Definition() && (aProcessName == "G4RadioactiveDecayBase" || aProcessName == "none"))
+		{
+			// G4cout << "energy: " << aTrack->GetKineticEnergy()/keV << " keV" << G4endl;
+			man->FillNtupleDColumn(0, 0, aTrack->GetKineticEnergy()/keV); // [keV]
+		}
 	}
 }
-    
-  return fUrgent;
+
+return fUrgent;
 }
