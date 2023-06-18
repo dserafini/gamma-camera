77fc0feee39d8279437c7a74b14cc6dcc6375b02
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-18 11:37:36 +0200

    Update stacking.cc

--- stacking.cc
+++ stacking.cc
@@ -11,7 +11,7 @@ G4ClassificationOfNewTrack MyStackingAction::ClassifyNewTrack(
 	const G4Track* aTrack)
 {
 	// G4cout << "MyStackingAction::ClassifyNewTrack" << G4endl;
-	G4cout << "Particle: " << aTrack->GetParticleDefinition()->GetParticleName() << ", ";
+	// G4cout << "Particle: " << aTrack->GetParticleDefinition()->GetParticleName() << ", ";
 	if (aTrack->GetParticleDefinition() == G4OpticalPhoton::Definition())
 	{
 		G4cout << G4endl;
@@ -19,13 +19,13 @@ G4ClassificationOfNewTrack MyStackingAction::ClassifyNewTrack(
 	}
 	else
 	{
-		G4cout << "Kinetic energy: " << aTrack->GetKineticEnergy() / keV << " keV" << ", ";
+		// G4cout << "Kinetic energy: " << aTrack->GetKineticEnergy() / keV << " keV" << ", ";
 		
 		const G4VProcess *aProcess = aTrack->GetCreatorProcess();
 		G4String aProcessName = "none";
 		if (aProcess)
 			aProcessName = aProcess->GetProcessName();
-		G4cout << "from: " << aProcessName << G4endl;
+		// G4cout << "from: " << aProcessName << G4endl;
 		
 		G4AnalysisManager *man = G4AnalysisManager::Instance();
 		
