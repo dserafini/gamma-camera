8e04e11b340a1700fe97baa1c00c39b9608603ca
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-18 10:03:13 +0200

    Update stacking.cc

--- stacking.cc
+++ stacking.cc
@@ -2,11 +2,13 @@
 
 MyStackingAction::MyStackingAction()
 {
-  G4cout << "ClassifyNewTrack is set on aTrack->GetParentID() == 1" << G4endl;
+	G4cout << "ClassifyNewTrack is set on aTrack->GetParentID() == 1" << G4endl;
   
 	fMessengerPrimary = new G4GenericMessenger(this, "/primary/", "Primary selection for stacking action");
-  
+	
 	fMessengerPrimary->DeclareProperty("ion", fIonIsPrimary, "0 gamma, 1 ion");
+
+	fIonIsPrimary = 0;
 }
 
 MyStackingAction::~MyStackingAction()
@@ -18,7 +20,12 @@ G4ClassificationOfNewTrack MyStackingAction::ClassifyNewTrack(
   const G4Track* aTrack)
 {
   // G4cout << "MyStackingAction::ClassifyNewTrack" << G4endl;
-  
+
+G4AnalysisManager *man = G4AnalysisManager::Instance();
+
+if (fIonIsPrimary == 1)
+{
+G4cout << "save primary ion gamma daughter energy" << G4endl;
   if(aTrack->GetParentID() == 1) // Ag-111 daughter
     {
     
@@ -39,10 +46,20 @@ G4ClassificationOfNewTrack MyStackingAction::ClassifyNewTrack(
       {
         // G4cout << "gamma e figlia di Cd-111" << G4endl;
         // G4cout << "energy: " << aTrack->GetKineticEnergy()/keV << " keV" << G4endl;
-        G4AnalysisManager *man = G4AnalysisManager::Instance();
         man->FillNtupleDColumn(0, 0, aTrack->GetKineticEnergy()/keV); // [keV]
       }
     }
+}
+
+if (fIonIsPrimary == 0)
+{
+	// gammas and primaries
+	if(aTrack->GetDefinition() == G4Gamma::Definition() && aTrack->GetParentID() == 0)
+	{
+		G4cout << "save primary gamma energy" << G4endl;
+		man->FillNtupleDColumn(0, 0, aTrack->GetKineticEnergy()/keV);
+	}
+}
     
   return fUrgent;
 }
