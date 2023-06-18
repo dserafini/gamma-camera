71d6e136e6c7d24a4b814ffc440ea11dfd4f411e
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-18 21:25:10 +0200

    I remove the optical physics

--- physics.cc
+++ physics.cc
@@ -4,12 +4,8 @@ MyPhysicsList::MyPhysicsList()
 {
   // Decay Phisics
   RegisterPhysics(new G4DecayPhysics());
-  RegisterPhysics(new G4RadioactiveDecayPhysics());
   // EM 3
   RegisterPhysics(new G4EmStandardPhysics_option3());
-  //
-  G4OpticalPhysics * opticalPhysics = new G4OpticalPhysics();
-  RegisterPhysics(opticalPhysics);
 }
 
 MyPhysicsList::~MyPhysicsList()
