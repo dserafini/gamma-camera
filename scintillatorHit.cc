4bbc4a25ea278513688c6bc2c295992573d32f9b
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-27 12:06:17 +0200

    Update scintillatorHit.cc

--- scintillatorHit.cc
+++ scintillatorHit.cc
@@ -1,9 +1,5 @@
 #include "scintillatorHit.hh"
 #include "G4UnitsTable.hh"
-#include "G4VVisManager.hh"
-#include "G4Circle.hh"
-#include "G4Colour.hh"
-#include "G4VisAttributes.hh"
 
 #include <iomanip>
 
@@ -27,17 +23,6 @@ void scintillatorHit::Print()
 
 void scintillatorHit::Draw()
 {
-  G4VVisManager* pVVisManager = G4VVisManager::GetConcreteInstance();
-  if(pVVisManager)
-  {
-    G4Circle circle(fPos);
-    circle.SetScreenSize(4.);
-    circle.SetFillStyle(G4Circle::filled);
-    G4Colour colour(1.,0.,0.);
-    G4VisAttributes attribs(colour);
-    circle.SetVisAttributes(attribs);
-    pVVisManager->Draw(circle);
-  }
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
