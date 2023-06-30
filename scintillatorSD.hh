bb282bcc175dfe50116992082d419cbf0bdc2b7f
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-06-30 19:51:02 +0200

    Update scintillatorSD.hh

--- scintillatorSD.hh
+++ scintillatorSD.hh
@@ -1,30 +1,22 @@
-#ifndef SCINTILLATORSD_HH
-#define SCINTILLATORSD_HH 1
+#ifndef SCINTILLATOR_HH
+#define SCINTILLATOR_HH
 
 #include "G4VSensitiveDetector.hh"
-#include "g4root.hh" // ok with geant version 10.7
-// #include "g4root_defs.hh" // ok with geant version 11.1
+// #include "g4root.hh" // ok with geant version 10.7
+#include "g4root_defs.hh" // ok with geant version 11.1
 #include "G4RunManager.hh"
-#include "G4SDManager.hh"
 #include "G4SystemOfUnits.hh"
 
-#include "scintillatorHit.hh"
 
 class MySensitiveScintillator : public G4VSensitiveDetector
 {
 public:
-	MySensitiveScintillator(G4String, const G4String&);
+	MySensitiveScintillator(G4String);
 	~MySensitiveScintillator();
-	void Initialize(G4HCofThisEvent* hitCollection) override;
 
 private:
-	G4bool ProcessHits(G4Step*, G4TouchableHistory*) override;
-	void EndOfEvent(G4HCofThisEvent* hitCollection) override;
-	
-	scintillatorHitsCollection* fHitsCollection;
-	G4double fEdep;
-	G4ThreeVector fPosition;
-	G4int nofHits;
+  virtual G4bool ProcessHits(G4Step*, G4TouchableHistory*);
+
 };
 
 #endif
