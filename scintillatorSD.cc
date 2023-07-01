b933e879612ed2a3411c8f81608c2c1e9121a04b
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-07-01 18:07:30 +0200

    Update scintillatorSD.cc

--- scintillatorSD.cc
+++ scintillatorSD.cc
@@ -2,7 +2,7 @@
 
 MySensitiveScintillator::MySensitiveScintillator(G4String name) : G4VSensitiveDetector(name)
 {
-  G4cout << "MySensitiveScintillator::MySensitiveScintillator" << G4endl;
+  // G4cout << "MySensitiveScintillator::MySensitiveScintillator" << G4endl;
   fEdep = 0.;
   fPosition = G4ThreeVector(0.,0.,0.);
 }
@@ -12,7 +12,7 @@ MySensitiveScintillator::~MySensitiveScintillator()
 
 void MySensitiveScintillator::Initialize(G4HCofThisEvent*)
 {
-  G4cout << "MySensitiveScintillator::Initialize" << G4endl;
+  // G4cout << "MySensitiveScintillator::Initialize" << G4endl;
   fEdep = 0.;
   fPosition = G4ThreeVector(0.,0.,0.);
   return;
@@ -36,14 +36,14 @@ G4bool MySensitiveScintillator::ProcessHits(G4Step * aStep, G4TouchableHistory *
 
 void MySensitiveScintillator::EndOfEvent(G4HCofThisEvent*)
 {
-  G4cout << "MySensitiveScintillator::EndOfEvent" << G4endl;
+  // G4cout << "MySensitiveScintillator::EndOfEvent" << G4endl;
 
   // normalize fPosition on total energy deposit
   if (fEdep>0)
     fPosition /= fEdep;
   
-  G4cout << "Energy deposited: " << fEdep/keV << " keV" << G4endl;
-  G4cout << "Position: " << fPosition << " keV" << G4endl;
+  // G4cout << "Energy deposited: " << fEdep/keV << " keV" << G4endl;
+  // G4cout << "Position: " << fPosition << " keV" << G4endl;
   
   G4AnalysisManager *man = G4AnalysisManager::Instance();
 
