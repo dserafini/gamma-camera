fc5dfdc06c68c68a6acd2cb5f955a781aebebe40
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-07-01 17:27:56 +0200

    Update scintillatorSD.cc

--- scintillatorSD.cc
+++ scintillatorSD.cc
@@ -9,7 +9,7 @@ MySensitiveScintillator::MySensitiveScintillator(G4String name) : G4VSensitiveDe
 MySensitiveScintillator::~MySensitiveScintillator()
 {}
 
-void MySensitiveScintillator::Initialize()
+void MySensitiveScintillator::Initialize(G4HCofThisEvent*)
 {
   G4cout << "MySensitiveScintillator::Initialize" << G4endl;
   fEdep = 0.;
