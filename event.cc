f9ca956cb8d228c32dccb2ee3d742c493e62913f
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-07-02 10:11:59 +0200

    Update event.cc

--- event.cc
+++ event.cc
@@ -14,7 +14,7 @@ void MyEventAction::BeginOfEventAction(const G4Event*)
 
 void MyEventAction::EndOfEventAction(const G4Event*)
 {
-  // G4cout << "MyEventAction::EndOfEventAction" << G4endl;
+  G4cout << "MyEventAction::EndOfEventAction" << G4endl;
 
   G4AnalysisManager *man = G4AnalysisManager::Instance();
   man->AddNtupleRow(0);
