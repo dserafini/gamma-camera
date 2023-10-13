d4084f1a175bba3f7d7fb6ba7bab9b34d70a2d01
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-10-13 18:14:59 +0200

    Update generator.cc

--- source/generator.cc
+++ source/generator.cc
@@ -22,7 +22,7 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 	// G4cout << "energy: " << fParticleGPS->GetParticleEnergy() / eV << " eV" << G4endl;
 	
 	// fParticleGPS->SetParticlePosition(GenerateParticlePositionMOBY());
-	// fParticleGPS->SetParticlePosition(G4ThreeVector(1,1,1));
+	fParticleGPS->SetParticlePosition(G4ThreeVector(1,1,1));
 
 	// G4cout << "energy: " << fParticleGPS->GetParticleEnergy() << G4endl;
 	G4cout << "myPos: " << fParticleGPS->GetParticlePosition() << G4endl;
