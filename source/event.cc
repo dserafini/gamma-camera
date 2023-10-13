38fe9484b0722c82833e6710f909d8d197b6c621
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-10-13 19:18:58 +0200

    Update event.cc

--- source/event.cc
+++ source/event.cc
@@ -25,7 +25,8 @@ void MyEventAction::EndOfEventAction(const G4Event* event)
   {
     G4ThreeVector v1 = event->GetPrimaryVertex()->GetPosition();
     G4cout << "event: " << v1 << G4endl;
-    man->FillH3(idH3, v1.getX(), v1.getY(), v1.getZ());
+    // man->FillH3(idH3, v1.getX(), v1.getY(), v1.getZ());
+    man->FillH2(0, v1.getX(), v1.getY());
   }
   else
     G4cout << "no vertex" << G4endl;
