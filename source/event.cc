ee5e430d60f795d11235da38e1f7077c12e565f9
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-10-13 18:47:25 +0200

    Update event.cc

--- source/event.cc
+++ source/event.cc
@@ -15,8 +15,8 @@ void MyEventAction::BeginOfEventAction(const G4Event* )
 void MyEventAction::EndOfEventAction(const G4Event* event)
 {
   // G4cout << "MyEventAction::EndOfEventAction" << G4endl;
-  // if (event->GetPrimaryVertex())
-  //  G4cout << "event: " << event->GetPrimaryVertex()->GetPosition() << G4endl;
+  if (event->GetPrimaryVertex())
+    G4cout << "event: " << event->GetPrimaryVertex()->GetPosition() << G4endl;
 
   G4AnalysisManager *man = G4AnalysisManager::Instance();
   man->AddNtupleRow(0);
