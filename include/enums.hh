dd005a0eca6c675456d6ac7405273f4fadab0e67
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-12-12 11:38:47 +0100

    Create enums.hh
    enumerate items for the tuples

---
+++ include/enums.hh
@@ -0,0 +1,21 @@
+# ifndef ENUMS_HH
+# define ENUMS_HH 1
+
+namespace Tuples {
+  enum {
+    kGeneration,
+    kScintillator,
+    kPhotons,
+    kSiPM
+  };
+}
+
+namespace TGeneration {
+  enum {
+    kVertexX,
+    kVertexY,
+    kVertexZ
+  };
+}
+
+# endif
