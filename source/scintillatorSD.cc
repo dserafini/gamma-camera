e58f3fe62021830529219a2487c16e675a30b1b3
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2023-12-12 15:31:06 +0100

    Update scintillatorSD.cc

--- source/scintillatorSD.cc
+++ source/scintillatorSD.cc
@@ -46,11 +46,6 @@ void MySensitiveScintillator::EndOfEvent(G4HCofThisEvent*)
   // G4cout << "Position: " << fPosition << " keV" << G4endl;
   
   G4AnalysisManager *man = G4AnalysisManager::Instance();
-
-  man->FillNtupleDColumn(0, 1, fEdep/keV); // [keV]
-  man->FillNtupleDColumn(0, 2, fPosition.getX()/mm); // [mm]
-  man->FillNtupleDColumn(0, 3, fPosition.getY()/mm);
-  man->FillNtupleDColumn(0, 4, fPosition.getZ()/mm);
   
   man->FillNtupleDColumn(Tuples::kScintillator, TScintillator::kEdep, fEdep/keV); // [keV]
   man->FillNtupleDColumn(Tuples::kScintillator, TScintillator::kGammaX, fPosition.getX()/mm); // [mm]
