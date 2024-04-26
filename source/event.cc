2f806c02e509620a3b4d2f1a98ea7ae66c7dd0a2
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-04-26 11:32:35 +0200

    Update event.cc

--- source/event.cc
+++ source/event.cc
@@ -15,14 +15,14 @@ void MyEventAction::BeginOfEventAction(const G4Event* )
 
 void MyEventAction::EndOfEventAction(const G4Event* currentEvent)
 {
-  // G4cout << "MyEventAction::EndOfEventAction" << G4endl;
+  G4cout << "MyEventAction::EndOfEventAction" << G4endl;
   
   G4SDManager* fSDM = G4SDManager::GetSDMpointer();
   MySensitiveDetector *sdet = (MySensitiveDetector*)fSDM->FindSensitiveDetector("SensitiveDetector");
   
   if (sdet->ShouldISaveEvent())
   {
-    // G4cout << "numberDetectedPhotons " << numberDetectedPhotons << G4endl;
+    G4cout << "numberDetectedPhotons " << numberDetectedPhotons << G4endl;
     
     G4AnalysisManager *man = G4AnalysisManager::Instance();
     man->AddNtupleRow(Tuples::kGeneration);
