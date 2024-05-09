0ded9f2cb4fe61c9f292fecf2cfcbc36ad3aacb1
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-05-09 13:46:24 +0200

    Update detectorSD.cc

--- source/detectorSD.cc
+++ source/detectorSD.cc
@@ -101,7 +101,9 @@ G4bool MySensitiveDetector::ProcessHits(G4Step* aStep, G4TouchableHistory*)
   newHit->SetEdep(edep);
   newHit->SetPos (aStep->GetPostStepPoint()->GetPosition());
   newHit->SetPixelPos (preStepPoint->GetTouchable()->GetTranslation());
-  // G4cout << aStep->GetPreStepPoint()->GetPhysicalVolume()->GetName() << ": " << aStep->GetPreStepPoint()->GetTouchable()->GetTranslation() << G4endl;
+  newHit->SetPixelIndexX (preStepPoint->GetTouchable()->SetCopyNumber(0));
+  newHit->SetPixelIndexY (preStepPoint->GetTouchable()->SetCopyNumber(1));
+  G4cout << aStep->GetPreStepPoint()->GetPhysicalVolume()->GetName() << ": " << newHit->GetPixelIndexX() << "\t" << newHit->GetPixelIndexY() << G4endl;
 
   if (fHitsCollection)
     fHitsCollection->insert( newHit );
