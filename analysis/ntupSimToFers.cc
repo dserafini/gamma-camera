9d68ffd2af42bc4003cb14f42c92709dc8222103
dserafini <124274535+dserafini@users.noreply.github.com>
2025-06-29 18:30:15 +0200

    trigger only on inner channels

--- analysis/ntupSimToFers.cc
+++ analysis/ntupSimToFers.cc
@@ -16,6 +16,37 @@ struct EventData {
     std::vector<UShort_t> val;
 };
 
+
+bool contieneChanValido(const std::vector<UShort_t>& chan,
+                        const std::vector<UShort_t>& val,
+                        UShort_t soglia) {
+							
+	// 36 inner channels list
+    static const std::unordered_set<UShort_t> valori_chan = {
+        5, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17, 19,
+        21, 22, 23, 24, 25, 27, 36, 38, 39, 40, 41,
+        42, 44, 46, 47, 48, 49, 50, 52, 54, 55, 56,
+        57, 58
+    };
+
+    size_t n = std::min(chan.size(), val.size());
+
+    for (size_t i = 0; i < n; ++i) {
+        if (valori_chan.count(chan[i]) && val[i] >= soglia) {
+            // std::cout << "Match: chan = " << chan[i] << ", val = " << val[i] << '\n';
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void moltiplicaVal(std::vector<UShort_t>& val, UShort_t fattore) {
+    for (auto& v : val) {
+        v *= fattore;
+    }
+}
+
 UShort_t XYtoFersChan(Int_t iX, Int_t iY) {
     // valori di X  dei canali 0-63   
     size_t const chID2mapX[64] = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 
@@ -101,6 +132,12 @@ void ntupSimToFers(TString file_in_name, TString file_out_name) {
     int evtcount=0;
     
     for (auto& [event, anEvent] : eventData) {
+		if (contieneChanValido(anEvent.chan, anEvent.val, valThre)) {
+			
+			// calibration
+			moltiplicaVal(anEvent.val, calFactor);
+
+			// fill tree
     	trigID = event;
 	trigTime = 0;
 	channelID = &(anEvent.chan);
